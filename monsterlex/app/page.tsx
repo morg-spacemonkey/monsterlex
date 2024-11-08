bd892455ae6e34d3cdddc70f21cf58397e9a68f3
morg-spacemonkey <morgane.lg@live.it>
2024-11-08 20:38:09 +0100

    per ora

--- monsterlex/app/page.tsx
+++ monsterlex/app/page.tsx
@@ -1,100 +1,106 @@
+'use client';
 import Image from "next/image";
+import Monsters from '../../data/owned.json';
+import {useEffect, useState} from "react";
+
+interface Monster {
+  Foto: string;
+  Nome: string;
+  Logo: Array<string>;
+  Sfondo: Array<string>;
+  Linguetta: string;
+  Piena: boolean;
+  Stato: number;
+}
 
 export default function Home() {
+  const [query, setQuery] = useState<string>('');
+  const [Logo, setLogo] = useState<string>('');
+  const [Sfondo, setSfondo] = useState<string>('');
+  const [monstersToVisualize, setMonstersToVisualize] = useState<Monster[]>(Monsters);
+
+useEffect(()=>{
+  let monstersData: Monster[] = Monsters;
+
+  if (query) {
+    monstersData = monstersData.filter((monster) =>{
+      return monster.Nome.toLowerCase().includes(query.toLowerCase());
+    });
+  }
+  setMonstersToVisualize(monstersData);
+
+}, [query]);
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <div>
+        <div>
+          Cerca: <br/>
+          <input type={'text'} value={query} onChange={(e) => setQuery(e.target.value)} className={'mb-20'}/>
         </div>
+
+        <div>
+          Filtra Logo: <br/>
+          <select defaultValue={0} onChange={(e) => setLogo((e.target.value))}>
+            <option value={''}>Tutti</option>
+            <option value={'giallo'}>Giallo</option>
+            <option value={'arancione'}>Arancione</option>
+            <option value={'pesca'}>Pesca</option>
+            <option value={'rosa'}>Rosa</option>
+            <option value={'rosso'}>Rosso</option>
+            <option value={'oro'}>Oro</option>
+            <option value={'argento'}>Argento</option>
+            <option value={'azzurro'}>Azzurro</option>
+            <option value={'blu'}>Blu</option>
+            <option value={'verde'}>Verde</option>
+            <option value={'bianco'}>Bianco</option>
+            <option value={'marrone'}>Marrone</option>
+            <option value={'beige'}>Beige</option>
+            <option value={'nero'}>Nero</option>
+          </select>
+        </div>
+        <div>
+          Filtra Sfondo: <br/>
+          <select defaultValue={0} onChange={(e) => setSfondo((e.target.value))}>
+            <option value={""}>Tutti</option>
+            <option value={'giallo'}>Giallo</option>
+            <option value={'arancione'}>Arancione</option>
+            <option value={'pesca'}>Pesca</option>
+            <option value={'rosa'}>Rosa</option>
+            <option value={'viola'}>Viola</option>
+            <option value={'rosso'}>Rosso</option>
+            <option value={'oro'}>Oro</option>
+            <option value={'azzurro'}>Azzurro</option>
+            <option value={'blu'}>Blu</option>
+            <option value={'verde'}>Verde</option>
+            <option value={'bianco'}>Bianco</option>
+            <option value={'marrone'}>Marrone</option>
+            <option value={'beige'}>Beige</option>
+            <option value={'panna'}>Panna</option>
+            <option value={'nero'}>Nero</option>
+          </select>
+        </div>
+      </div>
+
+      {monstersToVisualize.map((monster, index) => (
+        <div key={index} /* className={'bg-white p-10 mb-5 w-50 text-black'} */>
+          <div><Image src={monster.Foto} alt={'goblin'} width="200" height="400"/></div>
+          <div>
+          <h1>{monster.Nome}</h1>
+          <p>{monster.Linguetta}</p>
+          <p>{monster.Piena}</p>
+          <p>{monster.Stato}</p>
+          </div>
+        </div>
+      ))}
+
+
+
       </main>
+
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+        <p>Pretty cool tho</p>
       </footer>
     </div>
   );

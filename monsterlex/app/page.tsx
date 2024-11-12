de2407542160c828d7dc4cb039ba933e71aeb38b
morg-spacemonkey <morgane.lg@live.it>
2024-11-12 22:57:23 +0100

    ok

--- monsterlex/app/page.tsx
+++ monsterlex/app/page.tsx
@@ -19,6 +19,7 @@ export default function Home() {
   const [Sfondo, setSfondo] = useState<string>('');
   const [monstersToVisualize, setMonstersToVisualize] = useState<Monster[]>(Monsters);
 
+
 useEffect(()=>{
   let monstersData: Monster[] = Monsters;
 
@@ -29,18 +30,15 @@ useEffect(()=>{
   }
   setMonstersToVisualize(monstersData);
 
+
 }, [query]);
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-      <div>
-        <div>
+        <div style={{margin:'auto'}}>
           Cerca: <br/>
-          <input type={'text'} value={query} onChange={(e) => setQuery(e.target.value)} className={'mb-20'}/>
-        </div>
-
-        <div>
+          <input type={'text'} value={query} onChange={(e) => setQuery(e.target.value)} className={'mb-20'}/><br/>
           Filtra Logo: <br/>
           <select defaultValue={0} onChange={(e) => setLogo((e.target.value))}>
             <option value={''}>Tutti</option>
@@ -58,9 +56,7 @@ useEffect(()=>{
             <option value={'marrone'}>Marrone</option>
             <option value={'beige'}>Beige</option>
             <option value={'nero'}>Nero</option>
-          </select>
-        </div>
-        <div>
+          </select><br/>
           Filtra Sfondo: <br/>
           <select defaultValue={0} onChange={(e) => setSfondo((e.target.value))}>
             <option value={""}>Tutti</option>
@@ -79,18 +75,25 @@ useEffect(()=>{
             <option value={'beige'}>Beige</option>
             <option value={'panna'}>Panna</option>
             <option value={'nero'}>Nero</option>
-          </select>
+          </select><br/>
         </div>
-      </div>
 
       {monstersToVisualize.map((monster, index) => (
-        <div key={index} /* className={'bg-white p-10 mb-5 w-50 text-black'} */>
-          <div><Image src={monster.Foto} alt={'goblin'} width="200" height="400"/></div>
-          <div>
-          <h1>{monster.Nome}</h1>
-          <p>{monster.Linguetta}</p>
-          <p>{monster.Piena == true ? 'Piena' : 'Vuota'}</p>
-          <p>{monster.Stato}</p>
+        <div key={index} /* className={'bg-white p-10 mb-5 w-50 text-black'} */ style={{display: 'flex', flexDirection: 'row', margin:'auto'}}> 
+          <div><Image src={monster.Foto} alt={monster.Nome} width="150" height="300"/></div>
+          <div style={{margin:'auto'}}>
+          <h1><b>Nome:</b><br/>{monster.Nome}</h1>
+          <p><b>Linguetta:</b><br/>{monster.Linguetta}</p>
+          <p><b>Stato:</b><br/>{monster.Piena == true ? 'Piena' : 'Vuota'}</p>
+          <p><b>Conservazione:</b><br/>{(() => {
+                  switch (monster.Stato) {
+                    case 0:  return "Non impostato";
+                    case 1:  return "Perfetta";
+                    case 2:  return "Piccole Ammaccature";
+                    case 3:  return "Rovinata";
+                    default: return "Non impostato";
+                  }
+                })()}</p>
           </div>
         </div>
       ))}

715b48179dad6820a366dfa7674b58012209d6c0
morg-spacemonkey <morgane.lg@live.it>
2025-01-22 12:03:27 +0100

    dsfsad

--- app/page.tsx
+++ app/page.tsx
@@ -129,7 +129,7 @@ export default function Home() {
             <DataListRoot>
             <Box className={'flex flex-wrap gap-5'}>
                 <DataListItem label={'Collection'} value={monster.Collezione}/>
-                <DataListItem label={'Status'} value={monster.Piena}/>
+                <DataListItem label={'Status'} value={normalizeFullness(monster.Piena)}/>
               </Box>
               <Box className={'flex flex-wrap gap-5'}>
                 <DataListItem label={'Logo Color'} value={monster.Logo}/>
